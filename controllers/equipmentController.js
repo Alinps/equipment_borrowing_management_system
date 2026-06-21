f827988cf26a70ed9286bee85232bce1efa939a8
alin <alinps8406@gmail.com>
2026-06-21 14:03:32 +0530

    Add pagination and search functionality to listEquipment endpoint

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -127,11 +127,59 @@ const listEquipment = async (req, res ) => {
 
     try {
 
-        const equipments = await Equipment.find().sort({availableQuantity: -1});
+        const search = req.query.search || ''
+
+        const page = Number(req.query.page) || 1;
+
+        const limit = Number(req.query.limit) || 10;
+
+        const skip = (page - 1) * limit;
+
+        const query = {};
+
+
+        if (search) {
+
+            query.$or =  [
+                {
+                    name:{
+                        $regex: search,
+                        $options: 'i'
+                    }
+                },
+                
+                {
+                     catogory:{
+                        $regex: search,
+                        $options: 'i'
+                    }
+                },
+                
+                {
+                     serialNumber:{
+                        $regex: search,
+                        $options: 'i'
+                    }
+                }
+            ]
+        }
+
+
+        const totalRecords = await Equipment.countDocuments(query);
+
+
+        const equipments = await Equipment.find(query)
+                                          .sort({availableQuantity: -1})
+                                          .skip(skip)
+                                          .limit(limit);
 
         return res.status(200).json({
+
             success: true,
+            currentPage: page,
+            totalPages: Math.ceil(totalRecords/limit),
             count: equipments.length,
+            totalRecords,
             data: equipments
         })
 
